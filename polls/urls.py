da7b9ca3743163ff885f81d655eef6137c7524d7
BarriosSazo <BarriosSazo@BarriosSazo-PC.lan>
2011-09-18 14:57:59 -0600

    parte 3

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns('polls.views',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
@@ -12,9 +10,10 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        
-    
-    (r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^$', 'index'),
+    (r'^(?P<poll_id>\d+)/$', 'detail'),
+    (r'^(?P<poll_id>\d+)/results/$', 'results'),
+    (r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
 
 
