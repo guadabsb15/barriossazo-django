445057a7e15f3174d5435d7aad0f5b31d429839f
BarriosSazo <BarriosSazo@BarriosSazo-PC.lan>
2011-09-18 19:56:26 -0600

    parte 4

--- polls/urls.py
+++ polls/urls.py
@@ -1,19 +1,23 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
 
-
-
-urlpatterns = patterns('polls.views',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                       
-    (r'^$', 'index'),
-    (r'^(?P<poll_id>\d+)/$', 'detail'),
-    (r'^(?P<poll_id>\d+)/results/$', 'results'),
-    (r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+urlpatterns = patterns('',
+    (r'^$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html')),
+    (r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html')),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
+    (r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
 )
 
 
